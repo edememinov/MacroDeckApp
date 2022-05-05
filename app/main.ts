9f168006ee7d0bb31a75edc40634475052c400eb
edememinov <edememinov@hotmail.com>
2022-05-04 19:39:18 -0700

    Fixed error when updating software

--- app/main.ts
+++ app/main.ts
@@ -253,14 +253,17 @@ ipcMain.on('getVersion', (event, data) => {
 
 
 function clearDirectory(directory){
-  fs.readdir(directory, (err, files) => {
-    if (err) throw err;
+  if(fs.existsSync(directory)){
+    fs.readdir(directory, (err, files) => {
+      if (err) throw err;
+    
+      for (const file of files) {
+        fs.unlink(path.join(directory, file), err => {
+          if (err) throw err;
+        });
+      }
+    });
+  }
   
-    for (const file of files) {
-      fs.unlink(path.join(directory, file), err => {
-        if (err) throw err;
-      });
-    }
-  });
 }
 
